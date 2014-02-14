8bb41dd9dbccee8a41c20828fe28c41559a04700
Guillaume Salmon <guisalmon@gmail.com>
2014-02-14 13:04:37 +0100

    Drawer pane populated

--- src/org/black_mesa/webots_remote_control/MainActivity.java
+++ src/org/black_mesa/webots_remote_control/MainActivity.java
@@ -1,15 +1,27 @@
 package org.black_mesa.webots_remote_control;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	private String[] mDrawerListItems;
+    private ListView mDrawerList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
+		
+        mDrawerListItems = getResources().getStringArray(R.array.drawer_list_array);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mDrawerListItems));
 	}
 
 	@Override
