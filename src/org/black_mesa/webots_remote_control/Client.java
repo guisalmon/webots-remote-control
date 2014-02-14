68e5c4b692949e106ab1f9a854e30d4907296df6
IljaKroonen <ilja.kroonen@gmail.com>
2014-02-14 10:08:59 +0100

    Added Client class

---
+++ src/org/black_mesa/webots_remote_control/Client.java
@@ -0,0 +1,14 @@
+package org.black_mesa.webots_remote_control;
+
+import java.net.InetAddress;
+
+public class Client implements Runnable {
+	private InetAddress address;
+	private int port;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
