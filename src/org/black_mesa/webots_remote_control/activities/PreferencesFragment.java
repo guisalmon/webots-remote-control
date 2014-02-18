adc673b8db89beb137917893817c4414c3fcb645
NotCassim <ketfi.cassim@gmail.com>
2014-02-18 15:12:16 +0100

    Creating a ip adress preference in Preferences fragment

--- src/org/black_mesa/webots_remote_control/activities/PreferencesFragment.java
+++ src/org/black_mesa/webots_remote_control/activities/PreferencesFragment.java
@@ -4,19 +4,20 @@ import org.black_mesa.webots_remote_control.R;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class PreferencesFragment extends PreferenceFragment {
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.preferences_fragment, container, false);
-	}
+	 @Override
+     public void onCreate(Bundle savedInstanceState) {
+	     super.onCreate(savedInstanceState);
+         // Can retrieve arguments from preference XML.
+         Log.i("args", "Arguments: " + getArguments());
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		}
-
+         // Load the preferences from an XML resource
+         addPreferencesFromResource(R.xml.fragmented_preferences_inner);
+	 }
+	
 }
