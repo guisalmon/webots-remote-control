2964f254509dd1e1c626dc495414b49915a121d9
NotCassim <ketfi.cassim@gmail.com>
2014-02-19 12:47:13 +0100

    Created a port preferences and a french translation

--- src/org/black_mesa/webots_remote_control/activities/PreferencesFragment.java
+++ src/org/black_mesa/webots_remote_control/activities/PreferencesFragment.java
@@ -5,9 +5,6 @@ import org.black_mesa.webots_remote_control.R;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class PreferencesFragment extends PreferenceFragment {
 	 @Override
@@ -15,9 +12,8 @@ public class PreferencesFragment extends PreferenceFragment {
 	     super.onCreate(savedInstanceState);
          // Can retrieve arguments from preference XML.
          Log.i("args", "Arguments: " + getArguments());
-
          // Load the preferences from an XML resource
-         addPreferencesFromResource(R.xml.fragmented_preferences_inner);
+         addPreferencesFromResource(R.xml.fragmented_preferences);
 	 }
 	
 }
