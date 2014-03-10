081ce15eda3d5008c2d42d3c6835277792b0b3e4
Guillaume Salmon <guisalmon@gmail.com>
2014-03-10 20:20:18 +0100

    Empty fragment class added (changes to come)

---
+++ src/org/black_mesa/webots_remote_control/activities/ConnexionFragment.java
@@ -0,0 +1,11 @@
+package org.black_mesa.webots_remote_control.activities;
+
+import android.app.Fragment;
+
+public class ConnexionFragment extends Fragment{
+
+	public ConnexionFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
